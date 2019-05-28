1c46d8b78460afc38bd08db7462493ec8ca145dc
Ignacio Gómez Sáez <nachoperegrino94@gmail.com>
2019-05-28 06:37:08 +0200

    Counter implemented

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,26 @@
-function Count (input) {
-  throw new Error('Not implemented')
+function Count(input) {
+  if (input < 0) {
+    // throw new RangerError("Please, input must be a positive number");
+    throw new RangeError()
+  }
+  // Array for the results of the function (with the number of 1's and the position)
+  var result = []
+  // To store the count of 1's and the position of the current bit checked
+  var countsum = 0
+  var pos = 0
+  // For storing what's left in our number through the function
+  var left = input
+
+  while (left > 0) {
+    if (left % 2 === 1) {
+      countsum++
+      result.push(pos)
+    }
+    pos++
+    left >>= 1
+  }
+  result.unshift(countsum)
+  return result
 }
 
 module.exports = { Count }
